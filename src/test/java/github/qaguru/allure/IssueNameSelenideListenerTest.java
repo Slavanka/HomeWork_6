b801f3a9839725ac95e775445160b9917d2a2cfc
viktoria.klisheva <vikmah2@gmail.com>
2022-01-12 01:44:26 +0300

    немношка AllureReports, вариант с Listener

---
+++ src/test/java/github/qaguru/allure/IssueNameSelenideListenerTest.java
@@ -0,0 +1,25 @@
+package github.qaguru.allure;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+
+public class IssueNameSelenideListenerTest {
+
+    @Test
+    public void testGithub() {
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("Slavanka/HomeWork_6");
+        $(".header-search-input").submit();
+
+        $(linkText("Slavanka/HomeWork_6")).click();
+        $(partialLinkText("Issues")).click();
+
+    }
+}
